143096ff3f9f86ab017531792b6e4e6c01911093
Tittoh <tittohb@gmail.com>
2023-01-26 04:25:18 +0300

    Add 7 segment BCD

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "@code-hike/mdx/dist/index.css"
 import '../styles/global.css';
 import '../styles/mandala.css';
 import "../styles/ch-styles.css";
+import "../styles/state-machine.css";
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
