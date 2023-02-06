78255b4461da54e2df8d8555c47dec4193652578
uvacoder <uva22940coder@gmail.com>
2023-02-06 18:52:16 -0500

    Update footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
   return <span className="text-xs">
-     © {new Date().getFullYear()} <a className="underline" href="https://twitter.com/_tittoh" target="_blank">Tittoh</a>
+     © {new Date().getFullYear()} <a className="underline" href="https://twitter.com/VaLandscapes" target="_blank">uvacoder</a>
   </span>
 }
