36867cceaf04877c66071615468c23abad0d427e
uvacoder <uva22940coder@gmail.com>
2023-02-08 22:43:48 -0500

    Update logo.tsx

--- components/layout/logo.tsx
+++ components/layout/logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <div className="flex items-center flex-nowrap gap-1">
-      <img className="w-6" src="/images/logo96.svg" alt="logo" />
+      <img className="w-6" src="/images/logo.svg" alt="logo" />
       <h3 className="font-bold text-xl">Snippets</h3>
     </div>
   )
